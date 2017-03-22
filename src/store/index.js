04748689814786ffa2499f906afd813c76e2931f
yjunoo <yjunoo@qq.com>
2017-03-23 00:16:19 +0800

    base-frame

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './actions'
+import mutations from './mutations'
+
+Vue.use(Vuex);
+
+const store = Vuex.Store({
+    state:{}
+});
+
+export default store;
